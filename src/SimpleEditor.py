8e781be6eb71a1c34fc7428a2e216285d9a4a7ff
msantl <msantl.ck@gmail.com>
2014-04-12 23:34:24 +0200

    Added SimpleEditor.py

---
+++ src/SimpleEditor.py
@@ -0,0 +1,65 @@
+from Tkinter import *
+from tkSimpleDialog import askstring
+from tkFileDialog   import asksaveasfilename
+
+from tkMessageBox import askokcancel
+
+class Quitter(Frame):
+    def __init__(self, parent=None):
+        Frame.__init__(self, parent)
+        self.pack()
+        widget = Button(self, text='Quit', command=self.quit)
+        widget.pack(expand=YES, fill=BOTH, side=LEFT)
+
+    def quit(self):
+        ans = askokcancel('Quit', "Do you really want to quit?")
+        if ans:
+            Frame.quit(self)
+
+class ScrolledText(Frame):
+    def __init__(self, parent=None, text='', file=None):
+        Frame.__init__(self, parent)
+        self.pack(expand=YES, fill=BOTH)
+        self.makewidgets()
+        self.settext(text, file)
+
+    def makewidgets(self):
+        sbar = Scrollbar(self)
+        text = Text(self, relief=SUNKEN)
+        sbar.config(command=text.yview)
+        text.config(yscrollcommand=sbar.set)
+        sbar.pack(side=RIGHT, fill=Y)
+        text.pack(side=LEFT, expand=YES, fill=BOTH)
+        self.text = text
+
+    def settext(self, text='', file=None):
+        if file:
+            text = open(file, 'r').read()
+        self.text.delete('1.0', END)
+        self.text.insert('1.0', text)
+        self.text.mark_set(INSERT, '1.0')
+        self.text.focus()
+
+    def gettext(self):
+        return self.text.get('1.0', END+'-1c')
+
+class SimpleEditor(ScrolledText):
+    def __init__(self, parent=None, file=None):
+        frm = Frame(parent)
+        frm.pack(fill=X)
+        Button(frm, text='Save',  command=self.onSave).pack(side=LEFT)
+        Quitter(frm).pack(side=LEFT)
+        ScrolledText.__init__(self, parent, file=file)
+        self.text.config(font=('monospace', 12, 'normal'))
+
+    def onSave(self):
+        filename = asksaveasfilename()
+        if filename:
+            alltext = self.gettext()
+            open(filename, 'w').write(alltext)
+
+if __name__ == '__main__':
+    try:
+        SimpleEditor(file=sys.argv[1]).mainloop()
+    except IndexError:
+        SimpleEditor().mainloop()
