63c25ae7af4d0e89e8f2437dd0b7b360e095f506
msantl <msantl.ck@gmail.com>
2014-05-16 12:10:18 +0200

    added binding to space

--- src/SimpleEditor.py
+++ src/SimpleEditor.py
@@ -20,16 +20,21 @@ class Quitter(Frame):
             Frame.quit(self)
 
 class ScrolledText(Frame):
-    def __init__(self, parent=None, text='', disabled=False, file=None):
+    def __init__(self, parent=None, text='', binding=None, disabled=False, file=None):
         Frame.__init__(self, parent)
-        self.makewidgets()
-        #if disabled:
-        #    self.text.config(state=DISABLED)
+        self.makewidgets(binding)
+        if disabled:
+            self.text.config(state=DISABLED)
         self.settext(text, file)
 
-    def makewidgets(self):
+    def makewidgets(self, binding):
         sbar = Scrollbar(self)
         text = Text(self, relief=SUNKEN)
+
+        if binding:
+            (key, function) = binding
+            text.bind(key, function)
+
         sbar.config(command=text.yview)
         text.config(yscrollcommand=sbar.set, font=('monospace', 12, 'normal'))
         sbar.pack(side=RIGHT, fill=Y)
@@ -47,59 +52,58 @@ class ScrolledText(Frame):
         return self.text.get('1.0', END+'-1c')
 
 class SimpleEditor(Frame):
-	def __init__(self, parent=None, file=None):
-		Frame.__init__(self, parent)
+    def __init__(self, parent=None, file=None):
+        Frame.__init__(self, parent)
 
-		# frame for save and quit buttons
-		frm = Frame(parent)
-		frm.pack(fill=X)
-		# save button
-		Button(frm, text='Save',  command=self.onSave).pack(side=LEFT)
-		# Load button
-		Button(frm, text='Load',  command=self.onLoad).pack(side=LEFT)
-		# quit button
-		Quitter(frm).pack(side=LEFT)
-		# input textbox
-		self.inputText = ScrolledText(parent, file=file)
-		self.inputText.pack(expand=YES, fill=BOTH)
-		# separator
-		Frame(height=2, bd=1, relief=SUNKEN).pack(fill=X, padx=5, pady=5)
-		# control textbox
-		self.outputText = ScrolledText(parent, disabled=True)
-		self.outputText.pack(expand=YES, fill=BOTH)
-		# set inputText and outputText size
-		self.inputText.text.config(height=20)
-		self.outputText.text.config(height=8)
-		# set focus on inputText
-		self.inputText.text.focus()
-		
-		# TODO bindati na space i na inputText
-		frm.bind("<Button-1>", self.predictNextWord)
+        # frame for save and quit buttons
+        frm = Frame(parent)
+        frm.pack(fill=X)
+        # save button
+        Button(frm, text='Save',  command=self.onSave).pack(side=LEFT)
+        # Load button
+        Button(frm, text='Load',  command=self.onLoad).pack(side=LEFT)
+        # quit button
+        Quitter(frm).pack(side=LEFT)
+        # input textbox
+        self.inputText = ScrolledText(parent, binding=("<space>", self.predictNextWord),  file=file)
+        self.inputText.pack(expand=YES, fill=BOTH)
+        # separator
+        Frame(height=2, bd=1, relief=SUNKEN).pack(fill=X, padx=5, pady=5)
+        # control textbox
+        self.outputText = ScrolledText(parent, disabled=False)
+        self.outputText.pack(expand=YES, fill=BOTH)
+        # set inputText and outputText size
+        self.inputText.text.config(height=20)
+        self.outputText.text.config(height=8)
+        # set focus on inputText
+        self.inputText.text.focus()
 
-	def onSave(self):
-		filename = asksaveasfilename()
-		if filename:
-			alltext = self.inputText.gettext()
-			open(filename, 'w').write(alltext)
+    def onSave(self):
+        filename = asksaveasfilename()
+        if filename:
+            alltext = self.inputText.gettext()
+            open(filename, 'w').write(alltext)
 
-	def onLoad(self):
-		self.filename = askopenfile()
-		
-	def predictNextWord(self, event):
-		text = self.inputText.gettext()
-		text = text.split()
-		sequence = (str(text[-2]), str(text[-1]))
-		self.outputText.settext(get_next_word_witten_bell(self.filename, sequence))
-		#print get_next_word_witten_bell(self.filename, sequence)
+    def onLoad(self):
+        self.filename = askopenfile()
 
-		
+    def predictNextWord(self, event):
+        text = self.inputText.gettext()
+        text = text.split()
+
+        if len(text) < 3:
+            return
+
+        sequence = (str(text[-2]), str(text[-1]))
+        self.outputText.settext(get_next_word_witten_bell(self.filename, sequence))
+
 if __name__ == '__main__':
-	try:
-		master = Tk()
-		master.wm_title("Predictive Typing System")
+    try:
+        master = Tk()
+        master.wm_title("Predictive Typing System")
 
-		editor = SimpleEditor(parent=master, file=sys.argv[1])
-		editor.mainloop()
-	except IndexError:
-		SimpleEditor().mainloop()
+        editor = SimpleEditor(parent=master, file=sys.argv[1])
+        editor.mainloop()
+    except IndexError:
+        SimpleEditor().mainloop()
 
