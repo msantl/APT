422997490ca38eb59de5ec8b9951de63a5860832
msantl <msantl.ck@gmail.com>
2014-06-04 18:08:09 +0200

    having problem with evaluation

--- src/evaluate.py
+++ src/evaluate.py
@@ -32,6 +32,8 @@ def calculate(smooth, ngram, train_set, eval_set):
 
     f_handle = open(eval_set)
 
+    probabilities = list()
+
     for sentence in f_handle.readlines():
         sentence_probability = 1.0
 
@@ -46,11 +48,19 @@ def calculate(smooth, ngram, train_set, eval_set):
             word_probability = smooth.get_word_probability(context, word)
             sentence_probability *= word_probability
 
-            print context, word, word_probability
+            # print context, word, word_probability
 
-        P *= sentence_probability
+        probabilities.append(sentence_probability)
+
+        # print sentence, sentence_probability
 
     f_handle.close()
+
+    total = sum(probabilities)
+
+    for p in probabilities:
+        if (p / total) > 0.0:
+            P *= p
 
     H = -1.0 / Wt * math.log(P, 2)
     PP = 2 ** H
@@ -77,4 +87,10 @@ calculate(KneserNey(4), 3, training, evaluate)
 
 print "Witten Bell, n = 3"
 calculate(Witten_Bell(4), 3, training, evaluate)
+
+print "Kneser Ney, n = 4"
+calculate(KneserNey(4), 4, training, evaluate)
+
+print "Witten Bell, n = 4"
+calculate(Witten_Bell(4), 4, training, evaluate)
 
